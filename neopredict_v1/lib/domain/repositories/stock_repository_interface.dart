9eb4bff1e0cce040f24dbb32e8b8a867e9669651
Nero <133521280+JoakoDeAstora@users.noreply.github.com>
2025-12-18 23:37:59 -0300

    Delete neopredict_v1 directory

--- neopredict_v1/lib/domain/repositories/stock_repository_interface.dart
+++
@@ -1,6 +0,0 @@
-import 'package:neopredict_v1/domain/entities/stock_data.dart';
-
-abstract class StockRepositoryInterface {
-  Future<List<StockData>> getHistoricalData(String ticker);
-  Future<List<StockData>> getPredictionData(String ticker);
-}
--- neopredict_v1/lib/presentation/providers/company_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:neopredict_v1/domain/entities/company.dart';
-
-// Defaults to SQM-B
-final selectedCompanyProvider = StateProvider<Company>((ref) {
-  return ipsaCompanies[0];
-});
